44b7e458d66fb27add794598ef85fabf29b321bd
Sara <sara.spreitzhofer@stud.fh-campuswien.ac.at>
2021-12-09 18:17:39 +0100

    animated jump, added class Game, added settings.py

--- main.py
+++ main.py
@@ -1,53 +1,89 @@
 import pygame
 import os  # to define path to the images
-
-# https://www.youtube.com/watch?v=jO6qQDNa2UY
-
-WIDTH, HEIGHT = 900, 500
-WHITE = (255, 255, 255)
-FPS = 60  # frames per second
-RUNNER_WIDTH, RUNNER_HEIGHT = 120, 120
-VIRUS_WIDTH, VIRUS_HEIGHT = 80, 80
-VELOCITY_VIRUS = 4
-
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))  # make new window of defined width & height
-pygame.display.set_caption("Corona Game")  # window title
-
-RUNNER_IMAGE = pygame.image.load(os.path.join('assets', "runner.png"))
-RUNNER = pygame.transform.scale(RUNNER_IMAGE, (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
-
-VIRUS_IMAGE = pygame.image.load(os.path.join('assets', "virus.png"))
-VIRUS = pygame.transform.scale(VIRUS_IMAGE, (VIRUS_WIDTH, VIRUS_HEIGHT))  # resize image
+from settings import *
 
 
-def draw_window(runner, virus):
-    WIN.fill(WHITE)  # RGB color for the window background, defined as constant
-    # coordinate system: (0,0) is top left
-    WIN.blit(RUNNER, (runner.x, runner.y))  # draw surface (pictures, text, ...) on the screen
-    WIN.blit(VIRUS, (virus.x, virus.y))
-    pygame.display.update()  # update changes
+class Game:
+    def __init__(self): # initialize game window etc
+        pygame.init()
+        pygame.mixer.init()
+        self.WIN = pygame.display.set_mode((WIDTH, HEIGHT))  # make new window of defined width & height
+        pygame.display.set_caption(TITLE)  # window title
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.playing = True
+        self.jumping = False
+        self.runner = pygame.Rect(10, 370, RUNNER_WIDTH, RUNNER_HEIGHT)
+        self.virus = pygame.Rect(810, 410, VIRUS_WIDTH, VIRUS_HEIGHT)
+        self.RUNNER_IMAGE = pygame.image.load(os.path.join('assets', "runner.png"))
+        self.RUNNER = pygame.transform.scale(self.RUNNER_IMAGE, (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
+        self.VIRUS_IMAGE = pygame.image.load(os.path.join('assets', "virus.png"))
+        self.VIRUS = pygame.transform.scale(self.VIRUS_IMAGE, (VIRUS_WIDTH, VIRUS_HEIGHT))  # resize image
+        self.VELOCITY_JUMP = 15
 
+    def new(self):      # start a new game
+        #all_sprites = pygame.sprite.Group()
+        self.run()
 
-def main():  # code that handles main game loop in pygame
-    runner = pygame.Rect(10, 370, RUNNER_WIDTH, RUNNER_HEIGHT)
-    virus = pygame.Rect(810, 410, VIRUS_WIDTH, VIRUS_HEIGHT)
-    clock = pygame.time.Clock()
-    print("hello")
-    run = True
+    def run(self):  # code that handles main game loop in pygame
+        while self.playing:  # game loop: open & close the window
+            self.clock.tick(FPS)  # controls speed of the while loop
+            self.events()
+            self.update()
+            self.draw()
 
-    while run:  # game loop: open & close the window
-        clock.tick(FPS)  # controls speed of the while loop
+    def update(self):   # game loop - update
+        #self.all_sprites.update()
+        pygame.display.update()  # update changes
+
+    def events(self):   # game loop -events
         for event in pygame.event.get():  # loop through list of all different events
             if event.type == pygame.QUIT:
-                run = False  # end while loop if user quits game (press x)
-            # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            #    jump(runner)
-            # https://www.youtube.com/watch?v=G8pYfkIajE8
-        virus.x -= VELOCITY_VIRUS  # move image VELOCITY_VIRUS pixel to the left in each frame
-        draw_window(runner, virus)
-    pygame.quit()  # quits pygame & closes the window
+                if self.playing:
+                    self.playing = False  # end while loop if user quits game (press x)
+                self.running = False
+            #if self.jumping is False and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            #    self.jumping = True
+            #if self.jumping:
+            #    self.jump()
+        user_input = pygame.key.get_pressed()  # list of currently pressed key(s)
+        if self.jumping is False and user_input[pygame.K_SPACE]:
+            self.jumping = True
+        if self.jumping:
+            self.jump()
+            #pygame.time.delay(50)  # slows down everything!
+        self.virus.x -= VELOCITY_VIRUS  # move image VELOCITY_VIRUS pixel to the left in each frame
 
 
-if __name__ == "__main__":
-    main()  # only run the game if we run this file directly (not if it was imported from somewhere else)
+    def draw(self): # game loop - draw
+        self.WIN.fill(WHITE)  # RGB color for the window background, defined as constant
+        # coordinate system: (0,0) is top left
+        self.WIN.blit(self.RUNNER, (self.runner.x, self.runner.y))  # draw surface (pictures, text, ...) on the screen
+        self.WIN.blit(self.VIRUS, (self.virus.x, self.virus.y))
 
+
+    def show_start_screen(self):    # game splash / start screen
+        pass
+
+    def show_go_screen(self):   # game over / continue
+        pass
+
+    def jump(self): # gehört dann in Klasse Player!!
+        self.runner.y -= self.VELOCITY_JUMP
+        self.VELOCITY_JUMP -= 1
+        if self.VELOCITY_JUMP < -15:
+            self.jumping = False
+            self.VELOCITY_JUMP = 15
+
+
+g = Game()
+#g.show_start_screen()
+while g.running:
+    g.new()
+    #g.show_go_screen()
+
+pygame.quit()   # quits pygame & closes the window
+
+
+# if __name__ == "__main__":
+#    main()  # only run the game if we run this file directly (not if it was imported from somewhere else)
