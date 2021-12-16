97e366a6254c9dd8c755922db7e8ab72fdc0afc9
Sara <sara.spreitzhofer@stud.fh-campuswien.ac.at>
2021-12-16 12:03:48 +0100

    endlos jump

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os  # to define path to the images
 from settings import *
 
 
-class Game: # test
-    def __init__(self): # initialize game window etc
+class Game:
+    def __init__(self):  # initialize game window etc
         pygame.init()
         pygame.mixer.init()
         self.WIN = pygame.display.set_mode((WIDTH, HEIGHT))  # make new window of defined width & height
@@ -15,15 +15,14 @@ class Game: # test
         self.playing = True
         self.jumping = False
         self.runner = Runner()
-        #self.runner = pygame.Rect(10, 370, RUNNER_WIDTH, RUNNER_HEIGHT)
-        #self.RUNNER_IMAGE = pygame.image.load(os.path.join('assets', "runner.png"))
-        #self.RUNNER = pygame.transform.scale(self.RUNNER_IMAGE, (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
+        # self.runner = pygame.Rect(10, 370, RUNNER_WIDTH, RUNNER_HEIGHT)
+        # self.RUNNER_IMAGE = pygame.image.load(os.path.join('assets', "runner.png"))
+        # self.RUNNER = pygame.transform.scale(self.RUNNER_IMAGE, (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
         self.virus = pygame.Rect(810, 410, VIRUS_WIDTH, VIRUS_HEIGHT)
         self.VIRUS_IMAGE = pygame.image.load(os.path.join('assets', "virus.png"))
         self.VIRUS = pygame.transform.scale(self.VIRUS_IMAGE, (VIRUS_WIDTH, VIRUS_HEIGHT))  # resize image
-        self.VELOCITY_JUMP = 15
 
-    def new(self):      # start a new game
+    def new(self):  # start a new game
         self.run()
 
     def run(self):  # code that handles main game loop in pygame
@@ -33,67 +32,69 @@ class Game: # test
             self.update()
             self.draw()
 
-    def update(self):   # game loop - update
-        #self.all_sprites.update()
+    def update(self):  # game loop - update
+        # self.all_sprites.update()
         pygame.display.update()  # update changes
 
-    def events(self):   # game loop -events
+    def events(self):  # game loop -events
         for event in pygame.event.get():  # loop through list of all different events
             if event.type == pygame.QUIT:
                 if self.playing:
                     self.playing = False  # end while loop if user quits game (press x)
                 self.running = False
-            #if self.jumping is False and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            # if self.jumping is False and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             #    self.jumping = True
-            #if self.jumping:
+            # if self.jumping:
             #    self.jump()
         user_input = pygame.key.get_pressed()  # list of currently pressed key(s)
         if self.jumping is False and user_input[pygame.K_SPACE]:
             self.jumping = True
         if self.jumping:
-            self.jump()
-            #pygame.time.delay(50)  # slows down everything!
+            self.runner.jump()
+            # pygame.time.delay(50)  # slows down everything!
         self.virus.x -= VELOCITY_VIRUS  # move image VELOCITY_VIRUS pixel to the left in each frame
-        if self.runner.rect.colliderect(self.virus): # detect collisions of two rectangles
+        if self.runner.rect.colliderect(self.virus):  # detect collisions of two rectangles
             print("You are infected!")
-            self.VIRUS.fill(TRANSPARENT)    # make virus transparent after collision
+            self.VIRUS.fill(TRANSPARENT)  # make virus transparent after collision
 
-    def draw(self): # game loop - draw
+    def draw(self):  # game loop - draw
         self.WIN.fill(WHITE)  # RGB color for the window background, defined as constant
         # coordinate system: (0,0) is top left
-        self.WIN.blit(self.runner.image, (self.runner.rect.x, self.runner.rect.y))  # draw surface (pictures, text, ...) on the screen
+        self.WIN.blit(self.runner.image,
+                      (self.runner.rect.x, self.runner.rect.y))  # draw surface (pictures, text, ...) on the screen
         self.WIN.blit(self.VIRUS, (self.virus.x, self.virus.y))
-        #self.all_sprites.draw(self.WIN)
+        # self.all_sprites.draw(self.WIN)
 
-    def show_start_screen(self):    # game splash / start screen
+    def show_start_screen(self):  # game splash / start screen
         pass
 
-    def show_go_screen(self):   # game over / continue
+    def show_go_screen(self):  # game over / continue
         pass
 
 
-class Runner(pygame.sprite.Sprite): # Runner is a pygame Sprite object
-    def __init__(self): # runs whenever a new object of this type is made
+class Runner(pygame.sprite.Sprite):  # Runner is a pygame Sprite object (Vererbung / Interface)
+    def __init__(self):  # runs whenever a new object of this type is made
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(self.pygame.image.load(os.path.join('assets', "runner.png")), (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join('assets', "runner.png")),
+                                            (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
         self.rect = pygame.Rect(10, 370, RUNNER_WIDTH, RUNNER_HEIGHT)
-
+        self.VELOCITY_JUMP = 15
 
     def jump(self):
-        self.runner.rect.y -= self.VELOCITY_JUMP
+        self.rect.y -= self.VELOCITY_JUMP
         self.VELOCITY_JUMP -= 1
         if self.VELOCITY_JUMP < -15:
-            self.jumping = False
+            Game.jumping = False    # funktioniert noch nicht
             self.VELOCITY_JUMP = 15
 
+
 g = Game()
-#g.show_start_screen()
+# g.show_start_screen()
 while g.running:
     g.new()
-    #g.show_go_screen()
+    # g.show_go_screen()
 
-pygame.quit()   # quits pygame & closes the window
-
+pygame.quit()  # quits pygame & closes the window
 
 # if __name__ == "__main__":
 #    main()  # only run the game if we run this file directly (not if it was imported from somewhere else)
