c0fb4941560645949f9d177e85284f060365683f
Sara <sara.spreitzhofer@stud.fh-campuswien.ac.at>
2021-12-16 11:37:54 +0100

    sprites test

--- main.py
+++ main.py
@@ -3,27 +3,27 @@ import os  # to define path to the images
 from settings import *
 
 
-class Game:
+class Game: # test
     def __init__(self): # initialize game window etc
         pygame.init()
         pygame.mixer.init()
         self.WIN = pygame.display.set_mode((WIDTH, HEIGHT))  # make new window of defined width & height
         pygame.display.set_caption(TITLE)  # window title
         self.clock = pygame.time.Clock()
-        #all_sprites = pygame.sprite.Group()  # creates new empty group for all sprites
+        self.all_sprites = pygame.sprite.Group()  # creates new empty group for all sprites
         self.running = True
         self.playing = True
         self.jumping = False
-        self.runner = pygame.Rect(10, 370, RUNNER_WIDTH, RUNNER_HEIGHT)
+        self.runner = Runner()
+        #self.runner = pygame.Rect(10, 370, RUNNER_WIDTH, RUNNER_HEIGHT)
+        #self.RUNNER_IMAGE = pygame.image.load(os.path.join('assets', "runner.png"))
+        #self.RUNNER = pygame.transform.scale(self.RUNNER_IMAGE, (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
         self.virus = pygame.Rect(810, 410, VIRUS_WIDTH, VIRUS_HEIGHT)
-        self.RUNNER_IMAGE = pygame.image.load(os.path.join('assets', "runner.png"))
-        self.RUNNER = pygame.transform.scale(self.RUNNER_IMAGE, (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
         self.VIRUS_IMAGE = pygame.image.load(os.path.join('assets', "virus.png"))
         self.VIRUS = pygame.transform.scale(self.VIRUS_IMAGE, (VIRUS_WIDTH, VIRUS_HEIGHT))  # resize image
         self.VELOCITY_JUMP = 15
 
     def new(self):      # start a new game
-        #all_sprites = pygame.sprite.Group()
         self.run()
 
     def run(self):  # code that handles main game loop in pygame
@@ -54,15 +54,16 @@ class Game:
             self.jump()
             #pygame.time.delay(50)  # slows down everything!
         self.virus.x -= VELOCITY_VIRUS  # move image VELOCITY_VIRUS pixel to the left in each frame
-        if self.runner.colliderect(self.virus): # detect collisions of two rectangles
+        if self.runner.rect.colliderect(self.virus): # detect collisions of two rectangles
             print("You are infected!")
             self.VIRUS.fill(TRANSPARENT)    # make virus transparent after collision
 
     def draw(self): # game loop - draw
         self.WIN.fill(WHITE)  # RGB color for the window background, defined as constant
         # coordinate system: (0,0) is top left
-        self.WIN.blit(self.RUNNER, (self.runner.x, self.runner.y))  # draw surface (pictures, text, ...) on the screen
+        self.WIN.blit(self.runner.image, (self.runner.rect.x, self.runner.rect.y))  # draw surface (pictures, text, ...) on the screen
         self.WIN.blit(self.VIRUS, (self.virus.x, self.virus.y))
+        #self.all_sprites.draw(self.WIN)
 
     def show_start_screen(self):    # game splash / start screen
         pass
@@ -70,13 +71,20 @@ class Game:
     def show_go_screen(self):   # game over / continue
         pass
 
-    def jump(self): # gehört dann in Klasse Player!!
-        self.runner.y -= self.VELOCITY_JUMP
+
+class Runner(pygame.sprite.Sprite): # Runner is a pygame Sprite object
+    def __init__(self): # runs whenever a new object of this type is made
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(self.pygame.image.load(os.path.join('assets', "runner.png")), (RUNNER_WIDTH, RUNNER_HEIGHT))  # resize image
+        self.rect = pygame.Rect(10, 370, RUNNER_WIDTH, RUNNER_HEIGHT)
+
+
+    def jump(self):
+        self.runner.rect.y -= self.VELOCITY_JUMP
         self.VELOCITY_JUMP -= 1
         if self.VELOCITY_JUMP < -15:
             self.jumping = False
             self.VELOCITY_JUMP = 15
-
 
 g = Game()
 #g.show_start_screen()
